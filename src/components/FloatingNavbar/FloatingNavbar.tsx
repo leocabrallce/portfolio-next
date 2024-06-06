19e7244b9187a446a1e26538de2dc2c1dc9f3dbb
Leonardo Cabral <leocabrallce@gmail.com>
2024-06-07 00:14:35 +0200

    programmatically navigation

--- src/components/FloatingNavbar/FloatingNavbar.tsx
+++ src/components/FloatingNavbar/FloatingNavbar.tsx
@@ -1,24 +1,24 @@
 "use client";
 import MenuButton from "@/components/MenuButton";
-import { Link } from "next-view-transitions";
+import { useRouter } from "next/navigation";
 
 import { useTheme } from "next-themes";
 import React, { useEffect, useState } from "react";
 import { cn } from "@/utils/classNames";
 
-type Props = {
-  items: {
-    name: string;
-    link: string;
-    icon?: JSX.Element;
-  }[];
-};
-
 type Theme = "light" | "dark" | "system";
 
-function FloatingNavbar({ items }: Props) {
+function FloatingNavbar() {
   const [visible, setVisible] = useState(false);
   const { theme, setTheme } = useTheme();
+  const router = useRouter();
+
+  const items = [
+    { name: "Home", link: "/" },
+    { name: "Projects", link: "/projects" },
+    { name: "Contact", link: "/contact" },
+  ];
+
   const usedTheme: Theme = theme as Theme || "system";
   const buttonClassNames = cn("relative z-50 gap-4 w-fit p-4 items-center justify-center space-x-4 border-2 cursor-pointer transition-colors", {
     "hover:border-primary-light dark:hover:border-primary-dark border-primary-dark dark:border-primary-light bg-primary-dark dark:bg-primary-light text-primary-light dark:text-primary-dark": !visible,
@@ -34,6 +34,7 @@ function FloatingNavbar({ items }: Props) {
     },
     "w-screen h-screen fixed top-0 right-0 transition-all backdrop-blur-sm bg-primary-light/25 dark:bg-primary-dark/25"
   );
+  const buttonClasses = "flex items-center justify-center py-4 border-b-2 transition-colors";
 
   // change html class based on theme
   useEffect(() => {
@@ -57,7 +58,11 @@ function FloatingNavbar({ items }: Props) {
     }
   }, [visible]);
 
-  const buttonClasses = "flex items-center justify-center py-4 border-b-2 transition-colors";
+  const handlePageChange = (link: string) => {
+    setVisible(false);
+    router.push(link);
+  };
+
 
   return (
     <div className="fixed top-8 right-8 z-40">
@@ -75,9 +80,17 @@ function FloatingNavbar({ items }: Props) {
           <div className="z-40 w-full h-full m-0 pt-32 bg-primary-light dark:bg-primary-dark text-primary-dark dark:text-primary-light">
 
             <div className="flex flex-col gap-4">
-              <div className="flex justify-center gap-16 border-tra">
+              <div className="flex flex-col w-fit pl-4 sm:pl-12 md:pl-24">
                 <button onClick={() => setTheme('light')} className={cn(buttonClasses, "border-primary-dark", { "hover:border-primary-light": theme !== "light" })}>Light Mode</button>
                 <button onClick={() => setTheme('dark')} className={cn(buttonClasses, "border-primary-light", { "hover:border-primary-dark": theme !== "dark" })}>Dark Mode</button>
+              </div>
+
+              <div className="flex flex-col gap-2 items-start w-full mt-16">
+                {items.map((item) => (
+                  <div onClick={() => handlePageChange(item.link)} key={item.link} className="flex items-center justify-center cursor-pointer gap-4 py-4 pl-4 sm:pl-12 md:pl-24 border-b-2 transition-colors border-primary-light dark:border-primary-dark hover:border-primary-dark dark:hover:border-primary-light">
+                    <span className="text-6xl font-title uppercase">{item.name}</span>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
@@ -87,4 +100,4 @@ function FloatingNavbar({ items }: Props) {
   );
 };
 
-export default FloatingNavbar;
+export default FloatingNavbar;;
