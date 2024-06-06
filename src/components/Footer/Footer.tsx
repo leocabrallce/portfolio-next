19f426594905d4f6f8b8e1a2200963f295fc3dc0
Leonardo Cabral <leocabrallce@gmail.com>
2024-06-07 00:36:28 +0200

    responsive footer

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -26,12 +26,12 @@ function Footer() {
   ];
 
   return (
-    <footer className="mx-8 mt-20 p-16 text-lg flex flex-row justify-between items-center bg-primary-dark dark:bg-primary-light text-primary-light dark:text-primary-dark">
+    <footer className="mx-8 mt-20 p-16 text-lg flex flex-col-reverse md:flex-row justify-between items-start md:items-center gap-16 bg-primary-dark dark:bg-primary-light text-primary-light dark:text-primary-dark">
       <p className="text-center">
         &copy; {moment().format('YYYY')} Leonardo Cabral
       </p>
 
-      <div className='flex flex-row gap-4'>
+      <div className='flex flex-col md:flex-row gap-4'>
         {links.map((link) => (
           <Link key={link.link} href={link.link} className=' hover:text-primary underline' target='_blank'>
             {link.name}
